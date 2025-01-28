43de70c4ca061d841cb0a95e524153e96cfc61ed
NutanNimkar <nutanchandra067@gmail.com>
2025-01-28 16:36:08 -0500

    formatted backend and start some frontend

--- chat-frontend/src/components/Chat.tsx
+++ chat-frontend/src/components/Chat.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./Chat.module.css";
+import axios from "axios";
 
 const Chat = () => {
+  const [inputMessages, setInputMessages] = useState([]);
+  const [outputMessages, setOutputMessages] = useState([]);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Hello');
+  }
+
   return (
-    <div>Chat</div>
-  )
-}
+    <div className="chat">
+      <div className="chat-container">
+        <form onSubmit={handleSubmit} className="input-form">
+          <input
+            type="text"
+            placeholder="Type your message here"
+            required
+          />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default Chat;
